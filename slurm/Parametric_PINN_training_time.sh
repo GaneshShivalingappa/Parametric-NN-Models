3e4954e320882f9b1c6db33aa993be36fd9707a7
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2023-11-29 06:52:31 +0000

    created parametric sh

---
+++ slurm/Parametric_PINN_training_time.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=gpu_irmb
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=1D
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:ampere:1
+
+## Build command
+## singularity build --fakeroot --force parametric_nn.sif app/.devcontainer/container_conda.def
+
+SCRIPT=Parametric_PINN_training_time.py
+
+srun singularity run \
+ --cleanenv \
+ --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+ --nv \
+ --nvccli \
+ parametric_nn.sif \
+ python3 /home/y0103464/nn_models_project/app/${SCRIPT}
