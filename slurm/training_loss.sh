57d236f405e2acd6e7249c1657921716ffd8ed5f
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2024-03-07 14:21:31 +0000

    created training_loss slurm file

---
+++ slurm/training_loss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpu_irmb
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=1D
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:ampere:1
+
+## Build command
+## singularity build --fakeroot --force parametric_nn.sif app/Singularity/container_production.def
+
+srun singularity run \
+ --cleanenv \
+ --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+ --nv \
+ --nvccli \
+ --app training_loss \
+ parametric_nn.sif \
